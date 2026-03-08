6131d33bc1412472a1fa35ceeccca228e4d484ab
unknown <sebastian.vegar2015@gmail.com>
2026-03-07 21:40:32 -0500

    init
    inicio de estructura del proyecto

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, MapPin, Mail, Phone, Facebook, Instagram, Twitter } from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
+  readonly icons = { MapPin, Mail, Phone, Facebook, Instagram, Twitter };
 }
