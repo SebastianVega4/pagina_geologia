f61d9734059bf5f270af2540b66ba8f0078cd407
unknown <sebastian.vegar2015@gmail.com>
2026-03-08 15:44:14 -0500

    botton de whatsapp y noticias mejorada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,19 @@ import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { FooterComponent } from './core/components/footer/footer.component';
+import { LucideAngularModule, MessageCircle } from 'lucide-angular';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, LucideAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'web-congreso-geologia';
+  readonly icons = { MessageCircle };
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
@@ -27,5 +29,11 @@ export class AppComponent implements OnInit {
         easing: 'ease-in-out-cubic'
       });
     }
+  }
+
+  openWhatsApp() {
+    const phoneNumber = '573123456789';
+    const message = encodeURIComponent('Hola! Quisiera más información sobre el Congreso de Geología UPTC 2026.');
+    window.open(`https://wa.me/${phoneNumber}?text=${message}`, '_blank');
   }
 }
