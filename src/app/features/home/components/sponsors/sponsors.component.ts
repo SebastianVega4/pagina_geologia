4f47d328911ab28bcf77f073c10ad89d103a8de0
unknown <sebastian.vegar2015@gmail.com>
2026-05-21 22:55:25 -0500

    añadir imagenes
    leder, lideran y sponsor

--- src/app/features/home/components/sponsors/sponsors.component.ts
+++ src/app/features/home/components/sponsors/sponsors.component.ts
@@ -11,6 +11,9 @@ import { CommonModule } from '@angular/common';
 export class SponsorsComponent {
   sponsors = [
     { name: 'ARGOS', logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Argos_logo.png' },
-    { name: 'Geo Consultores', logo: 'assets/Geo_Consultores.png'}
-    ];
+    { name: 'Geo Consultores', logo: 'assets/Geo_Consultores.png' },
+    { name: 'Gobernación de Boyacá', logo: 'src\assets\Gobernacion_de_minas.png' },
+    { name: 'Agencia Nacional de Hidrocarburos', logo: 'src/assets/ANH.png' },
+    { name: 'Consejo Profesional de Geología', logo: 'src/assets/CPG_consejo_profesional_de_geologia.png' }
+  ];
 }
