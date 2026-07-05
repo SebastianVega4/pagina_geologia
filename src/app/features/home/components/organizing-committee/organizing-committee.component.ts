4d8ad002c609369fe51d4ed719323d6605dc711c
unknown <sebastian.vegar2015@gmail.com>
2026-07-05 18:08:47 -0500

    logos

--- src/app/features/home/components/organizing-committee/organizing-committee.component.ts
+++ src/app/features/home/components/organizing-committee/organizing-committee.component.ts
@@ -6,71 +6,107 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './organizing-committee.component.html',
-  styleUrl: './organizing-committee.component.scss'
+  styleUrl: './organizing-committee.component.scss',
 })
 export class OrganizingCommitteeComponent {
   docentes = [
     {
       role: 'Directora de la Escuela de Ingeniería Geológica',
       name: 'PhD Sandra Manosalva',
-      image: 'assets/Organizadores del Evento Docentes/Directora de la Escuela de Ingeniería Geológica PhD Sandra Manosalva.JPG'
+      image:
+        'assets/Organizadores del Evento Docentes/Directora de la Escuela de Ingeniería Geológica PhD Sandra Manosalva.JPG',
     },
     {
       role: 'Líder Comité de Docentes',
       name: 'PhD Jorge Mariño',
-      image: 'assets/Organizadores del Evento Docentes/Líder Comité de Docentes PhD Jorge Mariño.JPG'
+      image:
+        'assets/Organizadores del Evento Docentes/Líder Comité de Docentes PhD Jorge Mariño.JPG',
     },
     {
       role: 'Líder del Comité de Docentes',
       name: 'PhD Javier Idarraga',
-      image: 'assets/Organizadores del Evento Docentes/Líder del Comité de Docentes PhD Javier Idarraga.JPG'
-    }
+      image:
+        'assets/Organizadores del Evento Docentes/Líder del Comité de Docentes PhD Javier Idarraga.JPG',
+    },
   ];
 
   estudiantes = [
     {
       role: 'Presidenta',
       name: 'Angela Ramos',
-      image: 'assets/Organizadores del evento estudiantes/Presidenta Angela Ramos.JPG'
+      image:
+        'assets/Organizadores del evento estudiantes/Presidenta Angela Ramos.JPG',
     },
     {
       role: 'Representante de Ingeniería Geológica',
       name: 'Julián Robles',
-      image: 'assets/Organizadores del evento estudiantes/Representante de Ingeniería Geológica Julián Robles.JPG'
+      image:
+        'assets/Organizadores del evento estudiantes/Representante de Ingeniería Geológica Julián Robles.JPG',
     },
     {
       role: 'Vicepresidente y Secretario',
       name: 'Camilo Pedraza',
-      image: 'assets/Organizadores del evento estudiantes/Camilo Pedraza. Vicepresidente y Secretario.jpg'
+      image:
+        'assets/Organizadores del evento estudiantes/Camilo Pedraza. Vicepresidente y Secretario.jpg',
     },
     {
       role: 'Líder Comité Comunicaciones',
       name: 'Diego Acevedo',
-      image: 'assets/Organizadores del evento estudiantes/Líder Comité Comunicaciones Diego Acevedo.JPG'
+      image:
+        'assets/Organizadores del evento estudiantes/Líder Comité Comunicaciones Diego Acevedo.JPG',
     },
     {
       role: 'Lider Comité Logística',
       name: 'Manuela Zapata',
-      image: 'assets/Organizadores del evento estudiantes/Lider Comité Logística Manuela Zapata.JPG'
+      image:
+        'assets/Organizadores del evento estudiantes/Lider Comité Logística Manuela Zapata.JPG',
     },
     {
       role: 'Líder Comité Temáticos',
       name: 'Michelle Medina',
-      image: 'assets/Organizadores del evento estudiantes/Líder Comité Temáticos Michelle Medina.JPG'
+      image:
+        'assets/Organizadores del evento estudiantes/Líder Comité Temáticos Michelle Medina.JPG',
     },
     {
       role: 'Líder Comité Institucional',
       name: 'Ana Maria Castañeda',
-      image: 'assets/Organizadores del evento estudiantes/Líder Comité Institucional Ana Maria Castañeda.jpeg'
-    }
+      image:
+        'assets/Organizadores del evento estudiantes/Líder Comité Institucional Ana Maria Castañeda.jpeg',
+    },
   ];
 
   members = [
-    { name: 'UPTC', logo: 'assets/UPTC.png' },
-    { name: 'UPTC - Facultad de Seccional Sogamoso', logo: 'assets/Facultad Sogamoso.png' },
-    { name: 'Escuela de Ingeniería Geológica', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcdflFJKcrAfJ_GzdxbkhfxbxiXAyNBCJEsw&s' },
-    { name: 'Capítulo Estudiantil ACGP - UPTC', logo: 'assets/ACGGP.png' },
-    { name: 'Servicio Geológico Colombiano', logo: 'assets/Servicio_Geológico_Colombiano.png' },
-    { name: 'Sociedad Colombiana de Geología', logo: 'assets/Sociedad_Colombiana_de_geologia.png' }
+    {
+      name: 'UPTC',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/UPTC.png',
+    },
+    {
+      name: 'UPTC - Facultad de Seccional Sogamoso',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/LogoSogamoso.png',
+    },
+    {
+      name: 'Escuela de Ingeniería Geológica',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/EscuelaIngenieriaGeologica.png',
+    },
+    {
+      name: 'Capítulo Estudiantil ACGGP - UPTC',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/CapituloEstudiantilACGP.png',
+    },
+    {
+      name: 'Servicio Geológico Colombiano',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/Servicio_Geológico_Colombiano.png',
+    },
+    {
+      name: 'Sociedad Colombiana de Geología',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/SociedadColombianaDeGeologia.png',
+    },
+    {
+      name: 'Agencia Nacional de Minería',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/AgenciaNacionalDeMineria.png',
+    },
+    {
+      name: 'Sociedad Colombiana de Geotecnia',
+      logo: 'assets/INSTITUCIONES QUE LIDERAN/sociedadColombianaDeGeotecnia.png',
+    },
   ];
 }
--- src/app/features/home/components/sponsors/sponsors.component.ts
+++ src/app/features/home/components/sponsors/sponsors.component.ts
@@ -6,15 +6,49 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './sponsors.component.html',
-  styleUrl: './sponsors.component.scss'
+  styleUrl: './sponsors.component.scss',
 })
 export class SponsorsComponent {
   sponsors = [
-    { name: 'Agencia Nacional de Hidrocarburos', logo: 'assets/ANH.png' },
-    { name: 'Consejo Profesional de Geología', logo: 'assets/CPG_consejo_profesional_de_geologia.png' },
-    { name: 'ARGOS', logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Argos_logo.png' },
-    { name: 'Gobernación de Boyacá', logo: 'assets/Gobernacion_de_minas.png' },
-    { name: 'Geo Consultores', logo: 'assets/Geo_Consultores.png' },
-    { name: 'Concrelab', logo: 'assets/Concrelab.png '}
+    {
+      name: 'Agencia Nacional de Hidrocarburos',
+      logo: 'assets/INSTITUCIONES ALIADAS/AgenciaNacionalDeHidrocarburos.png',
+    },
+    {
+      name: 'Consejo Profesional de Geología',
+      logo: 'assets/INSTITUCIONES ALIADAS/CPG_consejo_profesional_de_geologia.png',
+    },
+    {
+      name: 'ARGOS',
+      logo: 'assets/INSTITUCIONES ALIADAS/ARGOS.png',
+    },
+    {
+      name: 'Gobernación de Boyacá',
+      logo: 'assets/INSTITUCIONES ALIADAS/GobernacionDeBoyacaDeMinas.png',
+    },
+    {
+      name: 'Geo Consultores',
+      logo: 'assets/INSTITUCIONES ALIADAS/Geo_Consultores.png',
+    },
+    {
+      name: 'Concrelab',
+      logo: 'assets/INSTITUCIONES ALIADAS/ConcreLab.png',
+    },
+    {
+      name: 'SUMICOL',
+      logo: 'assets/INSTITUCIONES ALIADAS/SUMICOL.jpeg',
+    },
+    {
+      name: 'La GeoTienda',
+      logo: 'assets/INSTITUCIONES ALIADAS/La GeoTienda.png',
+    },
+    {
+      name: 'MILPA',
+      logo: 'assets/INSTITUCIONES ALIADAS/MILPA.png',
+    },
+    {
+      name: 'PetroShale',
+      logo: 'assets/INSTITUCIONES ALIADAS/PetroShale.png',
+    },
   ];
 }
